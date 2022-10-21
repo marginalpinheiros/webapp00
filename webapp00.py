b67eca925a07e7f0fab5c0d0fca8a8878ff2a86b
marginalpinheiros <116382191+marginalpinheiros@users.noreply.github.com>
2022-10-21 16:04:03 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("whatsapp 2!")
+st.title("whatsapp 2")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Abacaxi em tudo!")
@@ -12,5 +12,7 @@ st.header("Abacaxi em tudo!")
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("bem vindo!")
 
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("um site para fazerem amigos!")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("um site para fazerem amigos!")
